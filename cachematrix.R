6af3604056cc2cc574d810950e30aaf71357f1e1
ssoneja <samridhsoneja@yahoo.com>
2014-12-20 19:42:43 -0600

    Updates to makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix: This function takes a matrix and creates its inverse 
+## if the inverse not present in the cache 
+## otherwise returns the inverse present in  the cache
+## cacheSolve: This function takes a matrix and checks if the inverse is
+## present in the cache. If the inverse is present, then it returns the values from 
+## the cache. If the inverse if not present then, it calculates the inverse using
+## the solve function, stores the inverse in the cache, and returns the inverse
 
-## Write a short comment describing this function
+## makeCacheMatrix: This function takes a matrix and creates its inverse 
+## if the inverse not present in the cache 
+## otherwise returns the inverse present in  the cache
 
 makeCacheMatrix <- function(x = matrix()) {
+        inv <- x
+        inv <- NULL
+        
+        get <- function() {x}
 
+        setinv <- function (inv) {inv <<- solve(x)}
+        getinv <- function () {inv}
+                
+        list(get=get,
+               setinv = setinv,
+               getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: This function takes a matrix and checks if the inverse is
+## present in the cache. If the inverse is present, then it returns the values from 
+## the cache. If the inverse if not present then, it calculates the inverse using
+## the solve function, stores the inverse in the cache, and returns the inverse
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m = matrix()
+        m <- x$getinv()
+        if(!is.null(m)){
+                message ("Getting cached data")
+                return (m)
+        }
+        
+        dat <- x$get()
+        m <- solve(dat, ...)
+        x$setinv(m)
+        return (m)
+        
 }
